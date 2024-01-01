4ed49f0fbb38d590d3468b96e97f48832f648bdc
andy <andycancode@gmail.com>
2023-12-31 16:35:59 -0800

    add box get, set methods

--- pkg/box/box.go
+++ pkg/box/box.go
@@ -1,6 +1,7 @@
 package box
 
 import (
+	"fmt"
 	"log"
 	"sync"
 	"time"
@@ -25,11 +26,52 @@ type Box struct {
 	logger log.Logger
 }
 
-func (b *Box) Get(key string) (Item, OperationError) {
+func (b *Box) Get(key string) (Item, error) {
 	b.mu.RLock()
-	defer b.mu.RUnlock()
+	item, found := b.data[key]
+	b.mu.RUnlock()
 
-	return Item{}, OperationError{}
+	if !found {
+		return Item{}, NewOperationError(fmt.Sprintf("Item with key %s doesn't exist", key), KeyNotFound)
+	}
+
+	if item.timeToLive > 0 {
+		elapsedTime := time.Since(item.creationTime)
+		if elapsedTime > item.timeToLive {
+			b.mu.Lock()
+			defer b.mu.Unlock()
+
+			delete(b.data, key)
+			expiredDuration := elapsedTime - item.timeToLive
+			return Item{}, NewOperationError(fmt.Sprintf("Item with key %s has expired %2.f seconds ago", key, expiredDuration.Seconds()), TTLExpired)
+		}
+	}
+
+	return item, nil
+
+}
+
+func (b *Box) Set(key string, value interface{}) error {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if len(b.data) > b.maxCapacity && b.maxCapacity > 0 {
+		evictedKey, err := b.evictStrat.Evict(b.data)
+		if err != nil {
+			return NewOperationError(err.Error(), Operational)
+		}
+		delete(b.data, evictedKey)
+	}
+
+	b.data[key] = Item{
+		key:          key,
+		value:        value,
+		lastUpdated:  time.Now(),
+		creationTime: time.Now(),
+		timeToLive:   b.defaultTTL,
+	}
+
+	return nil
 }
 
 type Option func(*Box)
--- pkg/box/error.go
+++ pkg/box/error.go
@@ -1,11 +1,15 @@
 package box
 
+import "errors"
+
 type ErrorType string
 
 const (
 	KeyNotFound      ErrorType = "KeyNotFound"
 	InvalidOperation           = "InvalidOperation"
 	Unauthorized               = "Unauthorized"
+	TTLExpired                 = "TTLExpired"
+	Operational                = "Operational"
 )
 
 type OperationError struct {
@@ -19,4 +23,11 @@ func (e OperationError) Error() string {
 
 func (e OperationError) Type() ErrorType {
 	return e.errType
+}
+
+func NewOperationError(errMsg string, errType ErrorType) *OperationError {
+	return &OperationError{
+		err:     errors.New(errMsg),
+		errType: errType,
+	}
 }
