771f6b8307a47076cb7234e18f93a09bd35f3bfe
andy <andycancode@gmail.com>
2023-12-31 20:58:27 -0800

    add item methods

--- pkg/box/box.go
+++ pkg/box/box.go
@@ -35,13 +35,10 @@ func (b *Box) Get(key string) (*Item, error) {
 		return &Item{}, NewOperationError(fmt.Sprintf("Item with key %s doesn't exist", key), KeyNotFound)
 	}
 
-	isTTLExpired, remainingTime := b.isTTLExpired(item)
-	if isTTLExpired {
+	if item.isExpired() {
 		delete(b.data, key)
 		return &Item{}, NewOperationError(fmt.Sprintf("Item with key %s has already expired", key), TTLExpired)
 	}
-
-	item.timeToLive = remainingTime
 
 	return item, nil
 }
@@ -52,11 +49,12 @@ func (b *Box) Set(key string, value interface{}) error {
 
 	item, found := b.data[key]
 	if found {
-		if isTTLExpired, remainingTime := b.isTTLExpired(item); !isTTLExpired {
+		if !item.isExpired() {
 			item.value = value
 			item.lastUpdated = time.Now()
-			item.timeToLive = remainingTime
+			return nil
 		}
+		delete(b.data, key)
 	}
 
 	if len(b.data) > b.maxCapacity && b.maxCapacity > 0 {
@@ -72,23 +70,9 @@ func (b *Box) Set(key string, value interface{}) error {
 		value:        value,
 		lastUpdated:  time.Now(),
 		creationTime: time.Now(),
-		timeToLive:   b.defaultTTL,
 	}
 
 	return nil
-}
-
-func (b *Box) isTTLExpired(item *Item) (bool, time.Duration) {
-	if item.timeToLive > 0 {
-		elapsedTime := time.Since(item.creationTime)
-		remainingTime := item.timeToLive - elapsedTime
-		if elapsedTime > item.timeToLive {
-			return true, 0
-		}
-
-		return false, remainingTime
-	}
-	return false, 0
 }
 
 type Option func(*Box)
--- pkg/box/item.go
+++ pkg/box/item.go
@@ -7,6 +7,40 @@ type Item struct {
 	value        interface{}
 	lastUpdated  time.Time
 	creationTime time.Time
-	timeToLive   time.Duration
+	setTTL       time.Duration
 	metaData     map[string]interface{}
+}
+
+func (i *Item) Key() string {
+	return i.key
+}
+
+func (i *Item) Value() interface{} {
+	return i.value
+}
+
+func (i *Item) LastUpdated() time.Time {
+	return i.lastUpdated
+}
+
+func (i *Item) CreationTime() time.Time {
+	return i.creationTime
+}
+
+func (i *Item) TTL() time.Duration {
+	elapsedTime := time.Since(i.creationTime)
+	remainingTime := i.setTTL - elapsedTime
+	if remainingTime < 0 {
+		remainingTime = 0
+	}
+
+	return remainingTime
+}
+
+func (i *Item) Metadata() map[string]interface{} {
+	return i.metaData
+}
+
+func (i *Item) isExpired() bool {
+	return i.TTL() == 0
 }
